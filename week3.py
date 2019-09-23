6799f5b9a4935c4ee091fcd737bb0715ac45f3ff
kaya0027 <sirribbellioglu@gmail.com>
2019-09-23 09:42:39 -0400

    kaya0027

---
+++ week3.py
@@ -0,0 +1,31 @@
+#This program calculates the circumference of a circle
+import math
+radius = float(input("What is the radius? > "))
+circumference =  2 * radius * math.pi
+print("Dear user the circumference is ?")
+print(circumference)
+
+from math import pi
+radius = float(input("What is the radius? > "))
+circumference =  2 * radius * pi
+print("Dear user the circumference is ?")
+print(circumference)
+
+#Question 8
+r=input("Please enter the radius value.");
+r_int=int(r);
+answer=2*pi* r_int;
+print("Answer is:");
+print(answer);
+#Question10
+Miles_string= input("Please insert the number of values.");
+Gallons_string=input("Please insert the number of gallons used.";
+ Miles_int=int(Miles_string);
+ Gallons_int=int(Miles_string);
+ Answer=Miles_int /Gallons_int;
+ print(Answer);
+#Question12
+Fah_string=input("Please enter the weather in fahrenheit");
+Fah_int=int(Fah_string);
+   Celcius= Fah_int / 2.16;
+    print(Celcius); print("Is the answer.");
